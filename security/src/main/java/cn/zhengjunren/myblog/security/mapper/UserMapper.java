aca519c520290dd386ea03b91cfd9cb386040f18
zzz1998 <1440629746@qq.com>
2019-12-15 22:48:42 +0800

    init success

--- security/src/main/java/cn/zhengjunren/myblog/security/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.zhengjunren.myblog.security.mapper;
-
-import cn.zhengjunren.myblog.security.domain.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
