5e21276414c0f708baa965a15c17db3c86c07da3
zzz1998 <1440629746@qq.com>
2019-12-27 20:00:19 +0800

    前端登录名更改

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -43,11 +43,6 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">用户名: admin</span>
-        <span> 密码: 123456</span>
-      </div>
-
     </el-form>
   </div>
 </template>
@@ -74,7 +69,7 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
+        username: process.env.VUE_APP_BASE_USERNAME,
         password: '123456'
       },
       loginRules: {
